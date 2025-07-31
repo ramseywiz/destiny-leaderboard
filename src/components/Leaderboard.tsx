206a845d3e457bc610a8e95368db364c9bbc7e64
ramseywiz <ramchandoklow@gmail.com>
2025-07-31 11:39:51 -0500

    small change to data for test

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,29 +1,25 @@
-import { AllCommunityModule, ModuleRegistry} from 'ag-grid-community';
 import type { ColDef } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
 import { useState } from 'react';
-
-ModuleRegistry.registerModules([AllCommunityModule]);
 
 
 export const Leaderboard = () => {
     // Row Data: The data to be displayed.
     const [rowData, setRowData] = useState([
-        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-        { make: "Ford", model: "F-Series", price: 33850, electric: false },
-        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        { name: "Vultorz", class: "Warlock", kills: 23, deaths: 300 },
+        { name: "Logers", class: "Titan", kills: 5932, deaths: 256 }
     ]);
 
     // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState<ColDef<any>[]>([
-        { field: "make" },
-        { field: "model" },
-        { field: "price" },
-        { field: "electric" }
+        { field: "name" },
+        { field: "class" },
+        { field: "kills" },
+        { field: "deaths" }
     ]);
 
-    return(
-        <div style={{ height:500, width: 800}}>
+    return (
+        <div className="ag-theme-alpine" style={{ height: 500 }}>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={colDefs}
