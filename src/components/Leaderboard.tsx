4b7f7faea389ad85be536b0c4c253cb2a6b2cb31
ramseywiz <ramchandoklow@gmail.com>
2025-07-31 20:30:49 -0500

    small stuff

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,16 +1,17 @@
 import type { ColDef } from 'ag-grid-community';
+import { themeMaterial } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
 import { useState } from 'react';
+import './Leaderboard.css'
+
 
 
 export const Leaderboard = () => {
-    // Row Data: The data to be displayed.
     const [rowData, setRowData] = useState([
-        { name: "Vultorz", class: "Warlock", kills: 23, deaths: 300 },
-        { name: "Logers", class: "Titan", kills: 5932, deaths: 256 }
+        { name: "Vultorz", class: "Warlock", kills: 23, deaths: true },
+        { name: "Logers", class: "Titan", kills: 5932, deaths: false }
     ]);
 
-    // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState<ColDef<any>[]>([
         { field: "name" },
         { field: "class" },
@@ -19,10 +20,11 @@ export const Leaderboard = () => {
     ]);
 
     return (
-        <div className="ag-theme-alpine" style={{ height: 500 }}>
+        <div className="leaderboard-div">
             <AgGridReact
                 rowData={rowData}
                 columnDefs={colDefs}
+                theme={themeMaterial}
             />
         </div>
     )
