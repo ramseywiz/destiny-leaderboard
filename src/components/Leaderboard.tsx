122d8befb559df34873b9c52691d4f6381abfc10
ramseywiz <ramchandoklow@gmail.com>
2025-09-25 16:12:35 -0500

    quick fix

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -7,6 +7,7 @@ import './Leaderboard.css'
 
 
 export const Leaderboard = () => {
+
     const [rowData, setRowData] = useState([
         { name: "Vultorz", class: "Warlock", kills: 23, deaths: true },
         { name: "Logers", class: "Titan", kills: 5932, deaths: false }
@@ -14,8 +15,8 @@ export const Leaderboard = () => {
 
     const [colDefs, setColDefs] = useState<ColDef<any>[]>([
         { field: "name", flex: 1 },
-        { field: "class",flex: 1 },
-        { field: "kills", flex: 1},
+        { field: "class", flex: 1 },
+        { field: "kills", flex: 1 },
         { field: "deaths", flex: 1 }
     ]);
 
