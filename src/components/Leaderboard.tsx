2825aeb6245c60872cb201af15b25be83cf3ab88
ramseywiz <ramchandoklow@gmail.com>
2025-10-01 12:57:39 -0500

    added a button flipper

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -4,16 +4,75 @@ import { AgGridReact } from 'ag-grid-react';
 import { useState } from 'react';
 import './Leaderboard.css'
 
-
-
 export const Leaderboard = () => {
     const [flipped, setFlipped] = useState<boolean>(false);
 
+    const data1 = [
+        {
+            name: "Vultorz",
+            class: "Warlock",
+            kills: 200000,
+            deaths: true
+        }
+    ]
+
+    const data2 = [
+        {
+            name: "Logers",
+            clan: "VEX",
+            kills: 2000,
+            persona: true
+        }
+    ]
+
+    const colD1 = [
+        {
+            field: "name",
+            flex: 1
+        },
+        {
+            field: "clan",
+            flex: 1
+        },
+        {
+            field: "kills",
+            flex: 1
+        },
+        {
+            field: "persona",
+            flex: 1
+        }
+    ]
+
+    const colD2 = [
+        {
+            field: "name",
+            flex: 1
+        },
+        {
+            field: "class",
+            flex: 1
+        },
+        {
+            field: "kills",
+            flex: 1
+        },
+        {
+            field: "deaths",
+            flex: 1
+        }
+    ]
+
     const flipData = () => {
         if (flipped) {
-            setFlipped(!flipped)
+            setFlipped(!flipped);
+            setColDefs(colD2);
+            setRowData(data1);
+
         } else {
             setFlipped(!flipped);
+            setColDefs(colD1);
+            setRowData(data2);
         }
     }
 
